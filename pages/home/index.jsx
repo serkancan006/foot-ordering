f5d26d0a3d577737a67137379457c4abc339c6e6
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-07-15 12:07:06 +0300

    Bölüm 2 sonu

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <div className='text-red-600 font-dancing text-center text-3xl'>Feane</div>
+    <div className='text-red-600 font-dancing text-center text-3xl'>Feanee</div>
   )
 }
 
