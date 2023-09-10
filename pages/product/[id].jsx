5bca03b4290bf86098d7a9edded0a20e4a08db24
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-09-10 06:42:43 +0300

    ders 90 sonu refactoring 1

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -12,6 +12,7 @@ const Index = ({ food }) => {
   const [extraItems, setExtraItems] = useState(food?.extraOptions);
   const [extras, setExtras] = useState([]);
   const cart = useSelector((state) => state.cart);
+  const findCart = cart.products.find((item) => item._id === food._id);
 
   const dispatch = useDispatch();
 
